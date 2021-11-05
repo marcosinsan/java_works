b7b12b73dacc5c35b282ae1788bc4eeaccb14896
Marco Santana <marcos.sinsan@gmail.com>
2021-11-05 17:13:43 -0300

    Add TopicoRepository

--- eclipse-workspace-dev/demo/src/main/java/br/com/demo/forum/repository/TopicoRepository.java
+++ eclipse-workspace-dev/demo/src/main/java/br/com/demo/forum/repository/TopicoRepository.java
@@ -1,9 +1,13 @@
 package br.com.demo.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.demo.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
 
 }
