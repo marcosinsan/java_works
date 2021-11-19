c2e5cbf87b57a9fceeef577070d96db3802b830b
Marco Santana <marcos.sinsan@gmail.com>
2021-11-19 16:37:46 -0300

    Liberando acesso aos endpoints

--- Java-pratica/java-spring-boot/src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
+++ Java-pratica/java-spring-boot/src/main/java/br/com/demo/forum/config/security/SecurityConfigurations.java
@@ -23,7 +23,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 	
 	// Configurações de recursos estaticos (JS, CSS, imagens, etc..) 
