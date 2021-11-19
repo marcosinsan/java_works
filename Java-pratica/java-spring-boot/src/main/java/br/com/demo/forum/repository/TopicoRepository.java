20047690affcf25df2fae12bd723cc5db83866e0
Marco Santana <marcos.sinsan@gmail.com>
2021-11-19 13:10:42 -0300

    Add dependencia de spring security e criando classe SecurityConfigurations

--- Java-pratica/java-spring-boot/src/main/java/br/com/demo/forum/repository/TopicoRepository.java
+++ Java-pratica/java-spring-boot/src/main/java/br/com/demo/forum/repository/TopicoRepository.java
@@ -1,7 +1,5 @@
 package br.com.demo.forum.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
