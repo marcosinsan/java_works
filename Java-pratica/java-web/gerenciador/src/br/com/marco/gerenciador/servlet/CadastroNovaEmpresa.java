849769dbcd859272e18eb311abc0ba3dfbfb8f82
Marco Santana <marcos.sinsan@gmail.com>
2022-01-14 20:52:20 -0300

    Criando um novo servlet que recebe um parametro.

---
+++ Java-pratica/java-web/gerenciador/src/br/com/marco/gerenciador/servlet/CadastroNovaEmpresa.java
@@ -0,0 +1,32 @@
+package br.com.marco.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CadastroNovaEmpresa
+ */
+@WebServlet("/novaEmpresa")
+public class CadastroNovaEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		PrintWriter out = response.getWriter();
+		String novaEmpresa = request.getParameter("nome");
+		
+		System.out.println("Empresa cadastrada com sucesso...");
+		out.println("<html><body>Empresa " + novaEmpresa + " cadastrada com sucesso!</body></html>");
+	}
+
+}
